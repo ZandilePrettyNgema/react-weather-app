befc2cfe50df6454b7f23636541b5e5f1fda0947
Zandile Pretty Ngema <zandilepretty01@gmail.com>
2024-03-21 02:09:46 +0200

    installed and imported bootstrap

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-      <form>
-        <input
-          type="Search"
-          placeholder="Enter a city..."
-          className="searchCity"
-        />
-        <input type="Submit" value="Search" className="btns" />
-        <input type="Submit" value="Current" className="btns" />
-      </form>
+      <div className="container">
+        <form>
+          <input
+            type="Search"
+            placeholder="Enter a city..."
+            className="searchCity"
+          />
+          <input type="Submit" value="Search" className="btns" />
+          <input type="Submit" value="Current" className="btns" />
+        </form>
 
-      <main>
-        <div className="WeatherInformation">
-          <h1>Empangeni</h1>
-          <span>Tuesday 1:20</span>
-          <h2>Clear</h2>
-        </div>
-      </main>
-      <WeatherConditions />
-      <WeatherForecast />
-      <Footer />
+        <main>
+          <div className="WeatherInformation">
+            <h1>Empangeni</h1>
+            <span>Tuesday 1:20</span>
+            <h2>Clear</h2>
+          </div>
+        </main>
+        <WeatherConditions />
+        <WeatherForecast />
+        <Footer />
+      </div>
     </div>
   );
 }
