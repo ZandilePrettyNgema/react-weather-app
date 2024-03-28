c8dddb72db53a9b35bba4239e940b7f142850c67
Zandile Pretty Ngema <zandilepretty01@gmail.com>
2024-03-28 03:49:22 +0200

    removed weather forecast and fahrenheit temperature

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,6 @@ import ReactAnimatedWeather from "react-animated-weather";
 import axios from "axios";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
-import WeatherTemperature from "./WeatherTemperature";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ dataReady: false });
@@ -63,86 +62,17 @@ export default function Weather(props) {
             <div className="WeatherIcon">
               <WeatherIcon code={weatherData.icon} size={52} />
             </div>
-            <WeatherTemperature celsius={weatherData.temperature} />
+            <h1 className="Temperature">
+              {Math.round(weatherData.temperature)}
+              <small className="DegreeSymbol">°C</small>
+            </h1>
           </div>
           <div className="WeatherConditionsData">
             <span>Humidity: {weatherData.humidity}%</span> <br />
             <span>Wind: {weatherData.wind}km/h</span>
           </div>
         </div>
-        <div className="WeatherForecast">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-2">
-                <div className="day">Mon</div>
-                <div className="icon">
-                  {" "}
-                  <ReactAnimatedWeather
-                    icon="CLEAR_DAY"
-                    color="#000"
-                    size={35}
-                    animate={true}
-                  />
-                </div>
-                <div className="temeperature">12°C</div>
-              </div>
-              <div className="row">
-                <div className="col-sm-2">
-                  <div className="day">Tue</div>
-                  <div className="icon">
-                    {" "}
-                    <ReactAnimatedWeather
-                      icon="CLEAR_NIGHT"
-                      color="#000"
-                      size={35}
-                      animate={true}
-                    />
-                  </div>
-                  <div className="temeperature">12°C</div>
-                </div>
-              </div>
-              <div className="col-sm-2">
-                <div className="day">Wed</div>
-                <div className="icon">
-                  {" "}
-                  <ReactAnimatedWeather
-                    icon="SNOW"
-                    color="#000"
-                    size={35}
-                    animate={true}
-                  />
-                </div>
-                <div className="temeperature">12°C</div>
-              </div>
-              <div className="col-sm-2">
-                <div className="day">Thur</div>
-                <div className="icon">
-                  {" "}
-                  <ReactAnimatedWeather
-                    icon="FOG"
-                    color="#000"
-                    size={35}
-                    animate={true}
-                  />
-                </div>
-                <div className="temeperature">12°C</div>
-              </div>
-              <div className="col-sm-2">
-                <div className="day">Fri</div>
-                <div className="icon">
-                  {" "}
-                  <ReactAnimatedWeather
-                    icon="CLEAR_DAY"
-                    color="#000"
-                    size={35}
-                    animate={true}
-                  />
-                </div>
-                <div className="temeperature">12°C</div>
-              </div>
-            </div>
-          </div>
-        </div>
+       
       </div>
     );
   } else {
